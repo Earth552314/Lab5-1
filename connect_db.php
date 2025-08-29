91f395b714eee43fccd58c3662770f92c8dc381e
Siraphop <siraphop.sir@ku.th>
2025-08-29 16:20:21 +0700

    Earth

---
+++ connect_db.php
@@ -0,0 +1,13 @@
+<?php
+$servername = "localhost";
+$username = "Siraphop";
+$password = "29042548sira";
+$db = "Siraphop";
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $db);
+// Check connection
+if (!$conn) {
+die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
+?>
